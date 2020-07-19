1cfc3d3b0f8848a794db7a35ca2d8326861b32b4
navya252001 <67997492+navya252001@users.noreply.github.com>
2020-07-19 18:40:46 +0530

    Add files via upload

---
+++ calc-js.js
@@ -0,0 +1,16 @@
+function clr() 
+         { 
+             document.getElementById("result").value = "" 
+         } 
+
+         function dis(val) 
+         { 
+             document.getElementById("result").value+=val 
+         } 
+
+         function solve() 
+         { 
+             var x = document.getElementById("result").value 
+             var y = eval(x) 
+             document.getElementById("result").value = y 
+         } 
